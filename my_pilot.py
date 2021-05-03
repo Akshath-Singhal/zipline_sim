1aea72cd1e86c9c1cb42dad15224ca9bb19ca99a
Akshath Singhal <akshaths@iiitd.ac.in>
2021-05-03 04:48:24 -0400

    WIP

--- my_pilot.py
+++ my_pilot.py
@@ -20,27 +20,43 @@ import pygame  # noqa
 TELEMETRY_STRUCT = struct.Struct(">Hhffb31B")
 COMMAND_STRUCT = struct.Struct(">fB3s")
 obstacle = []
-
+delivery = []
+roi = {}
 def compute_des_y(lidar, x_err, y_err):
 	if x_err < 80:
 		return 0
 	else:
 		fac = 0.0175
-		for i in range(len(lidar)):
-			if (lidar[i]<100):
-				loc = y_err + (i-15)*fac*lidar[i]
-				if (abs(lidar[i-1]-lidar[i])<10) or (abs(lidar[i+1]-lidar[i])<10):
-					obstacle.add([x_err - lidar[i],loc])
-					des = loc + 8
+		des = y_err
+		for i in lidar:
+			if i > 0:
+				x = x_err - i
+				y = int(y_err - (lidar.index(i)-15)*fac*i)
+				if x in roi.keys():
+					y_temp = roi.get(x)
+					y = y_temp.append(y)
 				else:
-					delivery.add([x_err - lidar[i], loc])
-					des = loc
+					y = [y]
+				roi[x]=y
+				print(roi, file = sys.stderr)
+		'''try:
+			m = min(i for i in lidar if i > 0)
+			i = lidar.index(m)
+			des = y_err + (i-15)*fac*lidar[i]
+			if (lidar[i]<100):
+				if (abs(lidar[(i-1)%31]-lidar[i])<4) or (abs(lidar[(i+1)%31]-lidar[i])<4):
+					obstacle.append([x_err - lidar[i],des])
+					des = des + 4
+				else:
+					delivery.append([x_err - lidar[i], des])
+		except:
+			des = y_err'''
 		return des
 
 def compute_lat_arspd(y_err, y_err_last, y_err_sum):
 	# PID Gains
 	kp = 5
-	kd = 3
+	kd = 5
 	ki = 0.2
 	desired = 20
 	lateral_arspd = float(kp*y_err - kd*(y_err-y_err_last) + ki*y_err_sum)
@@ -84,7 +100,6 @@ while True:
 	lat = compute_lat_arspd(y_err, y_err_last, y_err_sum)
 	y_err_last = y_err
 	y_err_sum = y_err_sum + y_err
-	print(data, lidar, file = sys.stderr)
 
 	command = COMMAND_STRUCT.pack(lat, drop, b'3s')
 	sys.stdout.buffer.write(command)
