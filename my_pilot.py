f09e3d75c9d3621d0c19720fbf651d1f65f00f37
Akshath Singhal <akshaths@iiitd.ac.in>
2021-05-03 02:26:50 -0400

    Work in progress

--- my_pilot.py
+++ my_pilot.py
@@ -5,7 +5,6 @@ import random
 import sys
 import subprocess
 import struct
-
 # Suppress hello from pygame so that stdout is clean
 os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
 import pygame  # noqa
@@ -20,13 +19,31 @@ import pygame  # noqa
 # 31 lidar samples [31 bytes]
 TELEMETRY_STRUCT = struct.Struct(">Hhffb31B")
 COMMAND_STRUCT = struct.Struct(">fB3s")
+obstacle = []
+
+def compute_des_y(lidar, x_err, y_err):
+	if x_err < 80:
+		return 0
+	else:
+		fac = 0.0175
+		for i in range(len(lidar)):
+			if (lidar[i]<100):
+				loc = y_err + (i-15)*fac*lidar[i]
+				if (abs(lidar[i-1]-lidar[i])<10) or (abs(lidar[i+1]-lidar[i])<10):
+					obstacle.add([x_err - lidar[i],loc])
+					des = loc + 8
+				else:
+					delivery.add([x_err - lidar[i], loc])
+					des = loc
+		return des
 
 def compute_lat_arspd(y_err, y_err_last, y_err_sum):
 	# PID Gains
-	kp = 4
-	kd = 1
-	ki = 0
-	lateral_arspd = float(kp*y_err + kd*(y_err-y_err_last) + ki*y_err_sum)
+	kp = 5
+	kd = 3
+	ki = 0.2
+	desired = 20
+	lateral_arspd = float(kp*y_err - kd*(y_err-y_err_last) + ki*y_err_sum)
 	return lateral_arspd
 
 def compute_drop(drop):
@@ -46,24 +63,28 @@ def validate_drop(flag):
 drop = 0
 y_err_last = 0
 y_err_sum = 0
-
+desired = 0
 while True:
 
 	abc = sys.stdin.buffer.read(TELEMETRY_STRUCT.size)
 	data = TELEMETRY_STRUCT.unpack(abc)
-	#print(data, file = sys.stderr)
 
 	if (data ==""):
 		continue
 	
 	[t_step, x_err, w_x, w_y, y_err] = data[0:5]
-	lidar = data[6:36]
+	lidar = data[5:36]
 
 	drop = compute_drop(drop)
+
+	desired = compute_des_y(lidar, x_err, y_err)
+
+	y_err = y_err - desired
 
 	lat = compute_lat_arspd(y_err, y_err_last, y_err_sum)
 	y_err_last = y_err
 	y_err_sum = y_err_sum + y_err
+	print(data, lidar, file = sys.stderr)
 
 	command = COMMAND_STRUCT.pack(lat, drop, b'3s')
 	sys.stdout.buffer.write(command)
